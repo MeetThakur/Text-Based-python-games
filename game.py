87bb3e262a2087358eaa8d9736f4dace93647c8e
VoiDxCode <99238677+VoiDxCode@users.noreply.github.com>
2022-05-07 12:26:55 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 from random import randint
 print("Welcome to the Game")
-print("Do You How To play? (y/n)")
-if input()=='n':
+print("Do You Want Tutorial? (press y if you want)")
+k = ("1","2","3","4","5","6","7","8","9","0")
+j = (1,2,3,4,5)
+if input()=='y':
     print("Tutorial : Computer Generates a random number \nYou Chose a number between 1 to 5 to subtract\nIf you make the number 1 you win\nNow Go and Play!")
 x = randint(30,60)
 r = 0
@@ -11,12 +13,20 @@ while x > 0:
     print("The Number Is",x)
     print("Your Turn")
     print("Enter a number Between from 1 to 5")
-    a = int(input())
-    while a > 5 or a <=0:
-        print("Please Enter a Appropriate Value")
-        a = int(input("Wrong Value, Enter value From 1 to 5: "))
-        if a <= 5 and a >=0:
+    a = (input())
+    if a in k:
+        a = int(a)
+    while a not in k:
+        print("Please Enter a Number not letters")
+        a = input("Wrong Value, Enter value From 1 to 5: ")
+        if a in k:
+            a = int(a)
+        if a in j:
             break
+    a = int(a)
+    while a >5 or a<1:
+        a = input("Wrong Value, Enter value From 1 to 5: ")
+        
     print("Your Choose : ",a)
     x = x - a
     if x == 1:
